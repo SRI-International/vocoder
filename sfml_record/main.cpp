da89c057d3dd87606682f6c698b995b4a6917adb
Matt Oyales <matt.oyales@sri.com>
2022-05-31 11:37:37 -0400

    Updating simple recorder application

--- sfml_record/main.cpp
+++ sfml_record/main.cpp
@@ -1,21 +1,42 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 #include <iostream>
+#include <string>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(640, 480, 32), "Recording Sounds");
+    // Create a 640 x 480 window with the specified string title
+    // "Simple Sound Recorder"
+    sf::RenderWindow window(sf::VideoMode(640, 240, 32), "Simple Sound Recorder");
 
-    // Ensure that there is a microphone
+    // Ensure that there is a microphone/recording device available
+    // If not, error out of the program.
     if (!sf::SoundBufferRecorder::isAvailable())
     {
-        std::cout << "You need a mic" << std::endl;
+        std::cerr << "Error: No available audio device detected." << std::endl;
         return 1;
     }
 
+    // Relevant sound components
     sf::SoundBufferRecorder recorder;
     sf::SoundBuffer buffer;
     sf::Sound sound;
+
+    // Text rendering components
+    // First load the font needed (ensure no errors)
+    sf::Font font;
+    if (!font.loadFromFile("arial.ttf"))
+    {
+        std::cerr << "Error: Font file could not be loaded." << std::endl;
+        return 1;
+    }
+    // Setting some simple GUI text to explain what you can do.
+    sf::Text text;
+    text.setFont(font);
+    text.setString
+        ("Press 'R' to record.\nPress 'S' to stop and save recording.\nPress 'P' to playback recent recording.\nPress 'Q' to quit.");
+    text.setCharacterSize(24);
+    text.setFillColor(sf::Color::White);
 
     while(window.isOpen())
     {
@@ -24,23 +45,34 @@ int main()
         {
             switch(Event.type)
             {
+            // Pressing 'X' in the window corner.
             case sf::Event::Closed:
                 window.close();
                 break;
+            // Define behavior for certain key presses.
             case sf::Event::KeyPressed:
+                // Recording audio
                 if (Event.key.code == sf::Keyboard::R)
                 {
-                    std::cout << "Recording start" << std::endl;
+                    std::cout << "Recording start. Begin speaking now..." << std::endl;
                     recorder.start();
+                }
+                // Stopping the recording 
+                else if (Event.key.code == sf::Keyboard::S)
+                {
+                    std::string filename = "output.ogg";
+                    std::cout << "Recording stopped." << std::endl;
+                    recorder.stop();
+                    buffer = recorder.getBuffer();
+                    buffer.saveToFile(filename); // Supports .wav, .ogg (not .mp3)
+                    std::cout << "Recording saved as '" << filename << "'." << std::endl;
                 }
                 else if (Event.key.code == sf::Keyboard::P)
                 {
-                    std::cout << "Recording stop" << std::endl;
-                    recorder.stop();
-                    buffer = recorder.getBuffer();
-                    buffer.saveToFile("test.ogg"); // Supports .wav, .ogg (not .mp3)
+                    std::cout << "Playing back your recording..." << std::endl;
                     sound.setBuffer(buffer);
                     sound.play();
+                    std::cout << "Playback done." << std::endl;
                 }
                 if (Event.key.code == sf::Keyboard::Q)
                 {
@@ -51,8 +83,10 @@ int main()
             }
         }
 
-        window.clear(sf::Color::Red);
+        window.draw(text);
+        // window.clear(sf::Color(0,0,0));
         window.display();
     }
+
     return 0;
 }
